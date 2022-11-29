634da8f1c3d1da05b123790f4cb5e126be93b861
breithner <breithnerap@gmail.com>
2022-11-28 22:22:16 -0500

    se agregó modelo subcategoria

--- inv/forms.py
+++ inv/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
-from .models import Categoria
+from .models import Categoria, SubCategoria
 
 class CategoriaForm(forms.ModelForm):
     class Meta:
@@ -16,3 +16,19 @@ class CategoriaForm(forms.ModelForm):
             self.fields[field].widget.attrs.update({
                 'class':'form-control'
             })
+
+class SubCategoriaForm(forms.ModelForm):
+    class Meta:
+        model =  SubCategoria
+        fields = ['categoria','descripcion','estado']
+        labels = {'descripcion':'Sub Categoria',
+                    'estado':'Estado'}
+        widget = {'descripcion':forms.TextInput}
+
+    def __init__(self,*args, **Kwargs):
+        super().__init__(*args, **Kwargs)
+        for field in iter(self.fields):
+            self.fields[field].widget.attrs.update({
+                'class':'form-control'
+            })
+        self.fields['categoria'].empty_label = 'Seleccione Categoria'
--- inv/views.py
+++ inv/views.py
@@ -4,8 +4,8 @@ from django.views import generic
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 
-from .models import Categoria
-from .forms import CategoriaForm
+from .models import Categoria, SubCategoria
+from .forms import CategoriaForm, SubCategoriaForm
 
 class CategoriaView(LoginRequiredMixin, generic.ListView):
     model =  Categoria
@@ -48,3 +48,24 @@ class CategoriaDel(LoginRequiredMixin, generic.DeleteView):
     context_object_name='obj'
     success_url=reverse_lazy("inv:categoria_list")
     # success_message="Categoría Eliminada Satisfactoriamente"
+
+
+class SubCategoriaView(LoginRequiredMixin, generic.ListView):
+    model =  SubCategoria
+    template_name = "inv/subcategoria_list.html"
+    context_object_name = "obj"
+    login_url = "bases:login"
+
+
+class SubCategoriaNew(LoginRequiredMixin, generic.CreateView):
+    # permission_required="inv.add_categoria"
+    model=SubCategoria
+    template_name="inv/subcategoria_form.html"
+    context_object_name = "obj"
+    form_class=SubCategoriaForm
+    success_url=reverse_lazy("inv:subcategoria_list")
+    # success_message="Categoria Creada Satisfactoriamente"
+
+    def form_valid(self, form):
+        form.instance.uc = self.request.user
+        return super().form_valid(form)
